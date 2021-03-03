7145c5f5b26dca1e31d4ac31970626eba85727bb
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2021-03-04 02:48:34 +0300

    small fix

--- lab_1/app.py
+++ lab_1/app.py
@@ -9,10 +9,20 @@ from watchdog.events import FileSystemEventHandler, FileCreatedEvent
     - другая отслеживает изменения в этой папке и копирует новые файлы из этой папки через буфер обмена в другую папку
 """
 
-source_path = "./test/source"
-target_path = "./test/target"
-font        = "arial"
-font_size   = 13
+safe_path    = "./test/user_safe"
+unsafe_path  = "./test/user_unsafe"
+villian_path = "./test/villain_safe"
+font         = "arial"
+font_size    = 12
+
+ID_FORMAT     = '[%Y-%m-%d_%H:%M:%S]'
+ID_FORMAT_TMP = '%Y-%m-%d_%H-%M-%S'
+
+def timestamp() -> str:
+    return time.strftime(ID_FORMAT, time.localtime())
+
+def timestamp_for_file() -> str:
+    return time.strftime(ID_FORMAT_TMP, time.localtime())
 
 
 class Handler(FileSystemEventHandler, QtCore.QObject):
@@ -22,7 +32,7 @@ class Handler(FileSystemEventHandler, QtCore.QObject):
         super(QtCore.QObject, self).__init__()
         self.observer = observer or None
 
-    def on_created(self, event: FileCreatedEvent):
+    def on_modified(self, event):
         if not event.is_directory:
             self.file_created.emit(event.src_path)
 
@@ -32,7 +42,7 @@ class FsObserver(QtCore.QThread):
         super().__init__(parent=parent)
         self._observer = Observer()
         self._handler = Handler()
-        self._observer.schedule(self._handler, path=source_path, recursive=True)
+        self._observer.schedule(self._handler, path=unsafe_path, recursive=True)
     
     def run(self) -> None:
         self._observer.start()
@@ -44,27 +54,39 @@ class Writer(QtWidgets.QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setup_ui()
-        self._pbCreateFile.clicked.connect(self.create_file)
+        self._pbCreateFile.clicked.connect(self.create_private_file)
+        self._pbCopyToPublic.clicked.connect(self.create_public_file)
 
     def setup_ui(self):
         self.setFont(QtGui.QFont(font, font_size))
         self._widget = QtWidgets.QWidget(self)
         self.setCentralWidget(self._widget)
         self._leFileName = QtWidgets.QLineEdit(self)
-        self._pbCreateFile = QtWidgets.QPushButton("Создать файл")
-        self._layout = QtWidgets.QHBoxLayout()
+        self._pbCreateFile = QtWidgets.QPushButton("Сохранить приватно")
+        self._layout = QtWidgets.QVBoxLayout()
         self._widget.setLayout(self._layout)
-        self._layout.addWidget(self._leFileName)
-        self._layout.addWidget(self._pbCreateFile)
+        self._buttons_layout = QtWidgets.QHBoxLayout()
+        self._layout.addLayout(self._buttons_layout)
+        self._buttons_layout.addWidget(self._leFileName)
+        self._buttons_layout.addWidget(self._pbCreateFile)
+        self._pbCopyToPublic = QtWidgets.QPushButton("Скопировать в открытую директорию", self)
+        self._layout.addWidget(self._pbCopyToPublic)
+        self._text_edit = QtWidgets.QTextEdit(self)
+        self._layout.addWidget(self._text_edit)
 
     def keyPressEvent(self, e: Qt.QKeyEvent):
         if e.key() == Qt.Qt.Key_Escape:
             self.close()
 
-    def create_file(self):
+    def create_private_file(self):
         file_name = self._leFileName.text()
-        with open(f"{source_path}/{file_name}", 'w') as f:
-            f.write("")
+        with open(f"{safe_path}/{file_name}", 'w') as f:
+            f.write(self._text_edit.toPlainText())
+
+    def create_public_file(self):
+        file_name = self._leFileName.text()
+        with open(f"{unsafe_path}/{file_name}", 'w') as f:
+            f.write(self._text_edit.toPlainText())
 
 
 class Reader(QtWidgets.QMainWindow):
@@ -74,11 +96,13 @@ class Reader(QtWidgets.QMainWindow):
         self._observer = FsObserver()
         self._observer._handler.file_created.connect(self.handle_file_creation)
         self._observer.start()
+        self.resize(QtCore.QSize(750, 250))
 
     def setup_ui(self):
         self.setFont(QtGui.QFont(font, font_size))
         self._table = QtWidgets.QListWidget(self)
-        self.setCentralWidget(self._table)
+        self._text_browser = QtWidgets.QTextBrowser(self)
+        self.setCentralWidget(self._text_browser)
 
     def keyPressEvent(self, e: Qt.QKeyEvent):
         if e.key() == Qt.Qt.Key_Escape:
@@ -92,13 +116,14 @@ class Reader(QtWidgets.QMainWindow):
         with open(source_file, 'r') as source:
             file_data = source.read()
         file_name = source_file.split('/')[-1]
-        target_file = f"{target_path}/{file_name}"
+        self.log_to_ui(f"Обнаружено сохранение: {file_name}")
+        target_file = f"{villian_path}/{timestamp_for_file()}_{file_name}"
         with open(target_file, 'w') as target:
             target.write(file_data)
-        self.log_to_ui(f"{source_file} -> {target_file}")
+        self.log_to_ui(f"Скопирован файл: {target_file}")
 
     def log_to_ui(self, msg: str):
-        self._table.addItem(msg)
+        self._text_browser.append(f"{timestamp()} {msg}")
 
 
 class App(Qt.QApplication):
@@ -107,12 +132,19 @@ class App(Qt.QApplication):
         self.setStyle('Fusion')
 
     def exec(self):
-        reader = Reader()
-        reader.show()
         writer = Writer()
         writer.show()
         return super().exec()
 
+class VillainApp(Qt.QApplication):
+    def __init__(self, argv=[]) -> None:
+        super().__init__(argv)
+        self.setStyle('Fusion')
+
+    def exec(self):
+        reader = Reader()
+        reader.show()
+        return super().exec()
 
 if __name__ == '__main__':
     App().exec()
---
+++ villain.py
@@ -0,0 +1,8 @@
+from lab_1.app import VillainApp
+
+
+def main():
+    return VillainApp().exec()
+
+if __name__ == '__main__':
+    main()
